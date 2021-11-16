ec6141ad554652f1e80224194b7d5f859e44b6d4
Steffo <bjoekeldude@gmail.com>
2021-11-16 08:26:39 +0100

    initial

---
+++ avhelper/avhelper.py
@@ -0,0 +1,6 @@
+def lib_fkt():
+	print("Ich bin eine Libraryfunktion")
+
+
+if __name__ == "__main__":
+	lib_fkt()
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'Artems-Loesungs-Lib'
+LONG_DESCRIPTION = 'Libary mit Funktionen die Artems Problem loesen.'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="avhelper", 
+        version=VERSION,
+        author="Stephan Bökelmann",
+        author_email="sb@gruppe.ai",
+        scripts=[],
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], 
+        url="",
+        
+        keywords=['python', 'debugging'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 3",
+            "Operating System :: POSIX",
+        ]
